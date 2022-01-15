62993a536d9f768be4db317ef62f7fcb5fb8348f
Malitha Liyanage <malitharanu@gmail.com>
2022-01-15 17:20:20 +0530

    Added normal navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {HomeModule} from "./modules/home/home.module";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
