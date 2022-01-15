53ea2bd8d346773760853b4f4b58250533c6040a
Malitha Liyanage <malitharanu@gmail.com>
2022-01-15 17:30:00 +0530

    Added a normal nav bar

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavBarComponent],
+  exports: [
+    NavBarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    LayoutModule
   ]
 })
 export class HomeModule { }
